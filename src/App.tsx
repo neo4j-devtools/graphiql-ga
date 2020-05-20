0cbd1a4d42a3735485cd5aea7b1d091a37e82727
ABK <akollegger@gmail.com>
2020-05-20 07:52:52 +0200

    added missing files :)

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import GraphiQL from 'graphiql';
+import 'graphiql/graphiql.min.css';
+const url = new URL(window.location.href);
+const apiEndpoint = url.searchParams.get('neo4jDesktopApiUrl');
+const apiClientId = url.searchParams.get('neo4jDesktopGraphAppClientId');
+
+const App = () => (
+  <GraphiQL
+    fetcher={async (graphQLParams:any) => {
+      const data = await fetch(
+        apiEndpoint || `${window.location.origin}/graphql`,
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'ClientId': apiClientId || 'missing client id'
+          },
+          body: JSON.stringify(graphQLParams),
+          credentials: 'same-origin',
+        },
+      );
+      return data.json().catch(() => data.text());
+    }}
+  />
+);
+
+export default App;
