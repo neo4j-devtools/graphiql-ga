e1b09620c87d5dec9b840fc182c16267141fd509
ABK <akollegger@gmail.com>
2020-05-21 08:08:13 +0200

    use different initial queries depending on configured endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,46 @@ import React from 'react';
 import GraphiQL from 'graphiql';
 import 'graphiql/graphiql.min.css';
 const url = new URL(window.location.href);
-const apiEndpoint = url.searchParams.get('neo4jDesktopApiUrl');
-const apiClientId = url.searchParams.get('neo4jDesktopGraphAppClientId');
+const relateApiEndpoint = url.searchParams.get('neo4jDesktopApiUrl');
+const relateApiClientId = url.searchParams.get('neo4jDesktopGraphAppClientId');
+
+const relateApiIsAvailable = (relateApiClientId !== null);
+
+const graphQLEndpoint = relateApiEndpoint || 'https://movies.grandstack.io/';
+
+const relateApiQuery = `query {
+  workspace {
+    projects {
+      name
+    }
+  }
+}`;
+
+const movieApiQuery = `query {
+  Movie(filter: {title_contains: "River Runs Through It"}) {
+    title
+    plot
+    year
+    actors {
+      name
+    }
+  }
+}
+`
+
+const initialGraphQLQuery = relateApiIsAvailable ? relateApiQuery : movieApiQuery;
 
 const App = () => (
   <GraphiQL
     fetcher={async (graphQLParams:any) => {
       const data = await fetch(
-        apiEndpoint || 'https://movies.grandstack.io/',
+        graphQLEndpoint,
         {
           method: 'POST',
           headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
-            'ClientId': apiClientId || 'missing client id'
+            'ClientId': relateApiClientId || 'missing client id'
           },
           body: JSON.stringify(graphQLParams),
           credentials: 'same-origin',
@@ -23,6 +49,7 @@ const App = () => (
       );
       return data.json().catch(() => data.text());
     }}
+    query={initialGraphQLQuery}
   />
 );
 
